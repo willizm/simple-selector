d330357814768176f496b1eacddebbc122df99d1
Bill <zhao.w@iiott.cn>
2014-07-09 11:09:09 +0800

    bug fix: * HTMLALLCollection is array-like object, for-in loop may return duplicated elements, caused by underlying object; * while selected no elements, els will be returned as an empty array, replace it with null;

--- src/selector.js
+++ src/selector.js
@@ -2,60 +2,62 @@
 ** @lib: Simple DOM Selector
 ** @usage: $(args), selecting JavaScript code should be under main DOM html, just before </body>
 ** args format supported: tagName#id, tagName.class, tagName, #id, .class
-** return: Array [element0, element1 ...]
+** @return: Array [element0, element1 ...] or null
 ** @author: willizm.cn@gmail.com
 */
-var $ = function (selector) {
+;(function(){
     "use strict";
-    // Check args
-    if (!/^[\-\w]*(#|\.)?[\-\w]*$/i.test(selector) || !selector) { return null; }
-    
-    // Add Array.prototype.indexOf method for lte IE8
-    if (!(Array.prototype.indexOf instanceof Function)) {
-        Array.prototype.indexOf = function(sth) {
-            for (var i = 0; i < this.length; i++) {
-                if (sth == this[i]) {
-                    return i;
+    window.$ = function (selector) {
+        // Check args
+        if (!/^[\-\w]*(#|\.)?[\-\w]*$/i.test(selector) || !selector) { return null; }
+
+        // Add Array.prototype.indexOf method for lte IE8
+        if (!(Array.prototype.indexOf instanceof Function)) {
+            Array.prototype.indexOf = function(sth) {
+                for (var i = 0; i < this.length; i++) {
+                    if (sth == this[i]) {
+                        return i;
+                    }
                 }
             }
         }
-    }
-    
-    var tag = {
-        "name": "",
-        "className": "",
-        "id": ""
-    },
-        elements = [],//element collection for deep selecting
-        els = [],//element collection for return
-        selstr = selector.toString();
-    
-    if (selstr.indexOf("#") >= 0) {
-        tag.name = selstr.split("#")[0];
-        tag.id =  selstr.split("#")[1];
-    } else if (selstr.indexOf(".") >= 0) {
-        tag.name = selstr.split(".")[0];
-        tag.className = selstr.split(".")[1];
-    } else {
-        tag.name = selstr;
-    }
-    
-    elements = (tag.name) ? (document.getElementsByTagName(tag.name)) : (document.all);
-    
-    if (tag.className) {
-        for (var i in elements) {
-            var el = elements[i];
-            if (el.className) {
-                var classes = (el.className).split(" ");
-                if (classes.indexOf(tag.className) >= 0) {
-                    els.push(el);
+
+        var tag = {
+            "name": "",
+            "className": "",
+            "id": ""
+        },
+            elements = {},//element collection for deep selecting
+            els = [],//element array for return
+            selstr = selector.toString();
+
+        if (selstr.indexOf("#") >= 0) {
+            tag.name = selstr.split("#")[0];
+            tag.id =  selstr.split("#")[1];
+        } else if (selstr.indexOf(".") >= 0) {
+            tag.name = selstr.split(".")[0];
+            tag.className = selstr.split(".")[1];
+        } else {
+            tag.name = selstr;
+        }
+                
+        if (tag.id) {
+            els[0] = document.getElementById(tag.id);
+        } else if (tag.className) {
+            elements = (tag.name) ? (document.getElementsByTagName(tag.name)) : (document.all);
+            for (var i = 0, l = elements.length; i < l; i++) {
+                var el = elements[i];
+                if(el.className !== undefined) {
+                    var classes = (el.className).split(" ");
+                    if (classes.indexOf(tag.className) >= 0) {
+                        els.push(el);
+                    }    
                 }
             }
+        } else {
+            els = document.getElementsByTagName(tag.name);
         }
-    } else if (tag.id) {
-        els[0] = document.getElementById(tag.id);
-    } else {
-        els = document.getElementsByTagName(tag.name);
+        return (els.length>0)?els:null;
     }
-    return els;
-}
+})()
+
